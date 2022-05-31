1af29a5281f7ea8c5550d50e8ce2a494af0a568e
Abhishek Yadav <92988351+Mysticguy-747@users.noreply.github.com>
2022-05-31 22:41:33 +0530

    Add files via upload

---
+++ bitwise.c
@@ -0,0 +1,45 @@
+// C program to get the nth bit of a number
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num, n, bitStatus;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    printf("Enter nth bit to check (0-31): ");
+    scanf("%d", &n);
+
+    bitStatus = (num >> n) & 1;
+
+    printf("The %d bit is set to %d", n, bitStatus);
+
+    return 0;
+}
+
+// C program to set the nth bit of a number
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num, n, newNum;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    printf("Enter nth bit to set (0-31): ");
+    scanf("%d", &n);
+
+    newNum = (1 << n) | num;
+
+    printf("Bit set successfully.\n\n");
+    printf("Number before setting %d bit: %d (in decimal)\n", n, num);
+    printf("Number after setting %d bit: %d (in decimal)\n", n, newNum);
+
+    return 0;
+}
